e0a45a03789edcf457a3b0b642110f4c03a6f718
egorkalinin <yahor.kalinin@gmail.com>
2023-01-13 17:35:48 +0300

    Add functions for every page

--- src/pages/CreateMeetup/CreateMeetup.tsx
+++ src/pages/CreateMeetup/CreateMeetup.tsx
@@ -4,6 +4,9 @@ import {
   Typography,
   TypographyComponent,
   TextField,
+  DateTimePicker,
+  ImageUploader,
+  ImagePreviewMode,
 } from 'components';
 import * as yup from 'yup';
 import { Formik, Form } from 'formik';
@@ -14,101 +17,157 @@ interface FormValues {
   [name: string]: String;
 }
 
+interface DateValues {
+  [name: string]: Date | null;
+}
+
+const descriptionText = () => {
+  return (
+    <div>
+      <Typography
+        className={styles.header}
+        component={TypographyComponent.Heading1}
+      >
+        Новый митап
+      </Typography>
+      <Typography
+        className={styles.paragraph}
+        component={TypographyComponent.Paragraph}
+      >
+        Заполните необходимые поля ниже наиболее подробно, это даст полную
+        информацию о предстоящем событии.
+      </Typography>
+    </div>
+  );
+};
+
+const requiredFields = ({ setConfirmed, index }: StepElementProps) => (
+  <div>
+    {descriptionText()}
+    <div className={styles.inputForm}>
+      <Formik
+        initialValues={{
+          title: '',
+          speaker: '',
+          description: '',
+        }}
+        validationSchema={yup.object().shape({
+          title: yup.string().required(`Это поле обязательно`),
+          speaker: yup.string().required(`Это поле обязательно`),
+          description: yup.string().required(`Это поле обязательно`),
+        })}
+        onSubmit={(values: FormValues, { setSubmitting }) => {
+          console.log(values);
+          setConfirmed(index, true);
+        }}
+      >
+        {({ values }) => (
+          <Form
+            autoComplete="off"
+            className={styles.formStyle}
+            onChange={() => {
+              if (
+                values.title !== '' &&
+                values.speaker !== '' &&
+                values.description !== ''
+              ) {
+                setConfirmed(index, true);
+              } else {
+                setConfirmed(index, false);
+              }
+              console.log(values.title, values.speaker, values.description);
+            }}
+          >
+            <TextField name="title" labelText="Название" multiline={false} />
+            <TextField name="speaker" labelText="Спикер" multiline={false} />
+            <TextField
+              name="description"
+              labelText="Описание"
+              multiline={true}
+            />
+          </Form>
+        )}
+      </Formik>
+    </div>
+  </div>
+);
+
+const additionalFields = ({ setConfirmed, index }: StepElementProps) => (
+  <div className="">
+    {descriptionText()}
+    <div className={styles.inputForm}>
+      <Formik<DateValues>
+        initialValues={{
+          startDate: null,
+          endDate: null,
+        }}
+        validationSchema={yup.object().shape({
+          startDate: yup.date().typeError('Select start date and time'),
+          endDate: yup.date().typeError('Select end date and time'),
+          place: yup.string().required(`Это поле обязательно`),
+          uploader: yup
+            .mixed()
+            .required(
+              'Пожалуйста, загрузите изображение формата .jpg, .jpeg или .png',
+            ),
+        })}
+        onSubmit={(values: DateValues, { setSubmitting }) => {
+          console.log(values);
+          setSubmitting(false); // onSubmit is sync, so need to call this
+        }}
+      >
+        {({ values }) => (
+          <Form
+            autoComplete="off"
+            className={styles.formStyle}
+            onChange={() => console.log(values)}
+          >
+            <ul className={styles.datePicker}>
+              <li className={styles.datePickerChild}>
+                <DateTimePicker name="startDate" labelText="Начало" />
+              </li>
+              <li className={styles.datePickerChild}>
+                <DateTimePicker name="endDate" labelText="Конец" />
+              </li>
+            </ul>
+            <div className={styles.placeField}>
+              <TextField
+                name="place"
+                labelText="Место проведения"
+                multiline={false}
+              />
+            </div>
+            <ImageUploader
+              name="uploader"
+              variant={ImagePreviewMode.Thumbnail}
+            />
+          </Form>
+        )}
+      </Formik>
+      <button onClick={() => setConfirmed(index, true)}>Click to pass</button>
+    </div>
+  </div>
+);
+
 export const CreateMeetup = () => {
   const steps = [
     {
       title: 'Обязательные поля',
-      element: ({ setConfirmed, index }: StepElementProps) => (
-        <div>
-          <Typography
-            className={styles.header}
-            component={TypographyComponent.Heading1}
-          >
-            Новый митап
-          </Typography>
-          <Typography
-            className={styles.paragraph}
-            component={TypographyComponent.Paragraph}
-          >
-            Заполните необходимые поля ниже наиболее подробно, это даст полную
-            информацию о предстоящем событии.
-          </Typography>
-          <div className={styles.inputForm}>
-            <Formik<FormValues>
-              initialValues={{
-                title: '',
-                speaker: '',
-                description: '',
-              }}
-              validationSchema={yup.object().shape({
-                title: yup.string().required(`Это поле обязательно`),
-                speaker: yup.string().required(`Это поле обязательно`),
-                description: yup.string().required(`Это поле обязательно`),
-              })}
-              onSubmit={(values: FormValues, { setSubmitting }) => {
-                setSubmitting(false); // onSubmit is sync, so need to call this
-              }}
-            >
-              {({ values, isValid, errors }) => (
-                <Form
-                  autoComplete="off"
-                  style={{
-                    width: '470px',
-                    maxWidth: '100%',
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    rowGap: '12px',
-                    margin: '0 auto',
-                    padding: '24px 0px 34px 0px',
-                  }}
-                >
-                  <TextField
-                    name="title"
-                    labelText="Название"
-                    multiline={false}
-                  />
-                  <TextField
-                    name="speaker"
-                    labelText="Спикер"
-                    multiline={false}
-                  />
-                  <TextField
-                    name="description"
-                    labelText="Описание"
-                    multiline={true}
-                  />
-                  {/* {isValid ? setConfirmed(index, true) : null} */}
-                </Form>
-              )}
-            </Formik>
-          </div>
-          <button onClick={() => setConfirmed(index, true)}>
-            Click to pass
-          </button>
-        </div>
-      ),
+      element: requiredFields,
     },
     {
       title: 'Дополнительные поля',
-      element: ({ setConfirmed, index }: StepElementProps) => (
-        <div>
-          <p>Some content of step 3</p>
-          <button onClick={() => setConfirmed(index, true)}>
-            Click to pass
-          </button>
-        </div>
-      ),
+      element: additionalFields,
     },
   ];
 
   let onFinishCheck = () => alert('Finish');
 
   return (
-    <div style={{ width: '550px' }}>
+    <div className={styles.wrapper}>
       <Stepper
         steps={steps}
-        finishButtonContent="Ready"
+        finishButtonContent="Создать"
         onFinish={onFinishCheck}
       />
     </div>
