e011f7e6da99a3149dff1bce148de0fde8821b29
egorkalinin <yahor.kalinin@gmail.com>
2023-01-30 23:40:55 +0300

    Add CreateMeetup exports

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -4,3 +4,6 @@ export * from './NewsPage/NewsPage';
 export * from './NotFoundPage/NotFoundPage';
 export * from './CreateMeetup/CreateMeetup';
 export * from './EditMeetupPage/EditMeetupPage';
+export * from './CreateMeetup/DescriptionText/DescriptionText';
+export * from './CreateMeetup/RequiredFields/RequiredFields';
+export * from './CreateMeetup/AdditionalFields/AdditionalFields';
