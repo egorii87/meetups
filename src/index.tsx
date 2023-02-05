816aa25c3fb6a854dbe5c3137e2fdf03236a4710
egorkalinin <yahor.kalinin@gmail.com>
2023-02-06 00:02:25 +0300

    Add IntlProvider for translate

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,27 @@ import ReactDOM from 'react-dom/client';
 import 'style/index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import messages_ru from './lang/ru.json';
+import messages_en from './lang/en.json';
+import { IntlProvider } from 'react-intl';
+
+const messages = {
+  ru: messages_ru,
+  en: messages_en,
+};
+
+//let language = navigator.language.split(/[-_]/)[0];
+
+const language = 'en';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <IntlProvider locale={navigator.language} messages={messages[language]}>
+      <App />
+    </IntlProvider>
   </React.StrictMode>,
 );
 
