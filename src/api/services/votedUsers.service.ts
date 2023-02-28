955108e1ee483fd6d0ef3bfa2efb7514fd2e59d6
egorkalinin <yahor.kalinin@gmail.com>
2023-03-01 00:26:11 +0300

    Update votedUsers.service

--- src/api/services/votedUsers.service.ts
+++ src/api/services/votedUsers.service.ts
@@ -1,7 +1,35 @@
-import { httpClient } from "helpers";
-import { ShortUser } from "model";
+import { httpClient } from 'helpers';
+import { ShortUser } from 'model';
 
 export const getVotedUsers = async (id: string): Promise<ShortUser[]> => {
-  const { data: votedUsers } = await httpClient.get<ShortUser[]>(`/meetups/${id}/votedusers`);
+  const { data: votedUsers } = await httpClient.get<ShortUser[]>(
+    `/meetups/${id}/votedusers`,
+  );
+  return votedUsers;
+};
+
+export const addVotingUser = async (
+  id: string,
+  votingUser: ShortUser,
+): Promise<ShortUser[]> => {
+  const { data: votedUsers } = await httpClient.post<ShortUser[]>(
+    `/meetups/${id}/votedusers`,
+    {
+      data: votingUser,
+    },
+  );
+  return votedUsers;
+};
+
+export const deleteVotedUser = async (
+  id: string,
+  votedUser: ShortUser,
+): Promise<ShortUser[]> => {
+  const { data: votedUsers } = await httpClient.delete<ShortUser[]>(
+    `/meetups/${id}/votedusers`,
+    {
+      data: votedUser,
+    },
+  );
   return votedUsers;
 };
