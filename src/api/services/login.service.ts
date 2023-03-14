a233e7ec79633ca79528a98a5f06a04ea2b8ba38
egorkalinin <yahor.kalinin@gmail.com>
2023-03-14 13:24:05 +0300

    If the user entered the username/password incorrectly or did not log in to the account return false

--- src/api/services/login.service.ts
+++ src/api/services/login.service.ts
@@ -1,14 +1,24 @@
 import { httpClient } from 'helpers';
 import { Credentials, User } from 'model';
 
-export const login = async (credentials: Credentials): Promise<User> => {
-  const resp = await httpClient.post('/login', credentials);
-  return resp.data.user as User;
+export const login = async (
+  credentials: Credentials,
+): Promise<User | boolean> => {
+  try {
+    const resp = await httpClient.post('/login', credentials);
+    return resp.data.user as User;
+  } catch {
+    return false;
+  }
 };
 
-export const checkLogin = async (): Promise<User> => {
-  const { data: authenticatedUser } = await httpClient.get<User>('/login');
-  return authenticatedUser;
+export const checkLogin = async (): Promise<User | boolean> => {
+  try {
+    const { data: authenticatedUser } = await httpClient.get<User>('/login');
+    return authenticatedUser;
+  } catch {
+    return false;
+  }
 };
 
 export const logout = async (): Promise<void> => {
