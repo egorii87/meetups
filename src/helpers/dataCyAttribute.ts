d5dad2a77e04acd0083de02fcc011eacadb96702
egorkalinin <yahor.kalinin@gmail.com>
2023-03-14 13:41:35 +0300

    Create an object with component names for e2e tests

---
+++ src/helpers/dataCyAttribute.ts
@@ -0,0 +1,13 @@
+export const dataCyAttribute = {
+  createMeetup: 'createMeetup',
+  login: 'login',
+  submitLogin: 'submitLogin',
+  meetupTitle: 'meetupTitle',
+  buttonNext: 'buttonNext',
+  inputDropBox: 'inputDropBox',
+  buttonFinish: 'buttonFinish',
+  news: 'news',
+  createNews: 'createNews',
+  submitNews: 'submitNews',
+  supportBuuton: 'supportBuuton',
+};
